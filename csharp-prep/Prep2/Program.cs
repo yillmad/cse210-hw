df61c4e6f64a78c2e6969b2b42829b28cbfe77b5
yillmad <90701683+yillmad@users.noreply.github.com>
2024-01-11 13:48:01 -0500

    first two

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,67 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        int grade = int.Parse(Console.ReadLine());
+        int reminder = grade%10;
+        string letter;
+        string sign;
+
+        // determine letter grade
+        if(grade >= 90 && grade <= 100)
+        {
+            letter = "A";
+        }
+        else if(grade >= 80 && grade < 100)
+        {
+            letter = "B";
+        }
+        else if(grade >= 70 && grade < 100)
+        {
+            letter = "C";
+        }
+        else if(grade >= 60 && grade < 100)
+        {
+            letter = "D";
+        }
+        else if(grade < 60)
+        {
+            letter = "F";
+        }
+        else{
+            letter ="X";
+        }
+
+        // + or -
+        if(reminder>=7){
+            sign = "+";
+        }
+        else if(reminder<3){
+            sign = "-";
+        }        
+        else{
+            sign = "";
+        }
+
+        if(letter=="A"&&sign=="+"){
+            sign = "";
+        }
+        else if(letter=="C"&&(sign=="+"||sign=="-")){
+            sign = "";
+        }
+
+        // Passed or not
+
+        if(letter=="A"||letter=="B"||letter=="C")
+        {
+            Console.WriteLine($"Your garde is {letter}{sign} and you passed");
+        }
+        else if(letter=="D"||letter=="F")
+        {
+            Console.WriteLine($"Your grade is {letter}{sign} and you didn't passed");
+        }
+        else{
+            Console.WriteLine("Invalid grade");
+        }
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+
+        if (int.TryParse(grade, out int intGrade))
+        {
+            string letter;
+
+            if (intGrade >= 90)
+            {
+                letter = "A";
+            }
+            else if (intGrade >= 80)
+            {
+                letter = "B";
+            }
+            else if (intGrade >= 70)
+            {
+                letter = "C";
+            }
+            else if (intGrade >= 60)
+            {
+                letter = "D";
+            }
+            else
+            {
+                letter = "F";
+            }
+
+            if (letter == "A" || letter == "B" || letter == "C")
+            {
+                Console.WriteLine($"Your grade is {letter} and you passed");
+            }
+            else if (letter == "D" || letter == "F")
+            {
+                Console.WriteLine($"Your grade is {letter} and you didn't pass");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid grade percentage.");
+        }
     }
 }
