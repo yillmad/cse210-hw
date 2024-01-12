e982bc9092159435ba81e42f00339d552319e79f
yillmad <90701683+yillmad@users.noreply.github.com>
2024-01-12 16:16:06 -0500

    third

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string toContinue = "yes";
+
+        while(toContinue=="yes"){
+            int guess;
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1,100);
+            int guesses = 0;
+
+            do{
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guesses++;
+
+                if (guess<magicNumber){
+                    Console.WriteLine("Higher");
+                }
+                else if (guess>magicNumber){
+                    Console.WriteLine("Lower");
+                }
+                else{
+                    Console.WriteLine("You guessed it!");
+                }
+
+                Console.WriteLine($"You made {guesses} guesses");
+            }while(guess!=magicNumber);
+
+            Console.WriteLine("Do you want to continue playing?");
+            toContinue = Console.ReadLine();
+        }
     }
 }
