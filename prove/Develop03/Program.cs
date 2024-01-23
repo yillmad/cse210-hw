8f969a036a229f025a0be71b796d321217c2c0c2
yillmad <90701683+yillmad@users.noreply.github.com>
2024-01-23 18:24:35 -0500

    prove 2

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Reference reference = new Reference("John", 3, 16);
+        Scripture scripture = new Scripture(reference, "For God so loved the world something else, I don't know what I have to write");
+
+        while (scripture.IsCompletelyHidden()==false)
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+
+            Console.WriteLine("\nPress Enter to hide more words or type 'quit' to exit:");
+            string userInput = Console.ReadLine();
+
+            if (userInput.ToLower() == "quit")
+            {
+                break;
+            }
+
+            scripture.HideRandomWords(2);
+        }
     }
 }
